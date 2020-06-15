1e97b88ce2b93c9e3b481c8d88a96621ead18dca
rkbhambri <ramkrishan@redbaron.in>
2020-06-15 18:05:00 +0530

    Updates

---
+++ precache-manifest.131c1cb83a8cc09c4433dd731b5a7b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa25badd60af60baa01ab597e3d2572c",
+    "url": "/image-size-converter/index.html"
+  },
+  {
+    "revision": "3a4433e66abe2a83e9e9",
+    "url": "/image-size-converter/static/css/main.65f62000.chunk.css"
+  },
+  {
+    "revision": "a2cfd89d41e6e149aa26",
+    "url": "/image-size-converter/static/js/2.804d9a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-size-converter/static/js/2.804d9a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4433e66abe2a83e9e9",
+    "url": "/image-size-converter/static/js/main.9e7b9382.chunk.js"
+  },
+  {
+    "revision": "945fea782be655e53a0b",
+    "url": "/image-size-converter/static/js/runtime-main.adc3cf00.js"
+  }
+]);
